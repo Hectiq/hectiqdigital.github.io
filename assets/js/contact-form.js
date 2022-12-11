c290934b3d1a1e2b93a5c5f4fdfe230e5ec51fab
Luca Pierabella <lpierabella@hectiq.games>
2022-12-11 19:51:50 +0100

    added 404. thanksyou page, integrated contact form

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -8,6 +8,8 @@ $(document).ready(function(){
         var user_name = $('input[name=name]').val();
         var user_email = $('input[name=email]').val();
         var user_message = $('textarea[name=message]').val();
+
+        var form = $('form[name=contact_form]')
         
         //simple validation at client's end
         //we simply change border color to red if empty field using .css()
@@ -35,24 +37,7 @@ $(document).ready(function(){
                 'userMessage': user_message
             };
             
-            //Ajax post data to server
-            $.post('contact_me.php', post_data, function(response){
-            
-                //load json data from server and output message     
-                if (response.type == 'error') {
-                    output = '<div class="error">' + response.text + '</div>';
-                }
-                else {
-                
-                    output = '<div class="success">' + response.text + '</div>';
-                    
-                    //reset values in all input fields
-                    $('#contact_form input').val('');
-                    $('#contact_form textarea').val('');
-                }
-                
-                $("#result").hide().html(output).slideDown();
-            }, 'json');
+            var jqxhr = form.post();
             
         }
         
